51fe26e83f05c8eeec8f1939002f9c745dd1e248
Jeroen Wijnholds <jeroenwijnholds@gmail.com>
2026-05-27 13:54:19 +0200

    feat: add AppFooter component

---
+++ components/AppFooter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const QUERY = `*[_type == "siteSettings"][0]{ siteName, footerCopyright, navigation }`
+
+interface NavItem { label: string; href: string; isButton: boolean }
+interface SiteSettings { siteName: string; footerCopyright: string; navigation: NavItem[] }
+
+const { data } = await useSanityQuery<SiteSettings>(QUERY)
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__container">
+      <NuxtLink to="/" class="footer__logo">
+        {{ data?.siteName }}
+      </NuxtLink>
+
+      <nav class="footer__nav" aria-label="Footer navigatie">
+        <ul>
+          <li v-for="item in data?.navigation?.filter((n: NavItem) => !n.isButton)" :key="item.href">
+            <NuxtLink :to="item.href">{{ item.label }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+
+      <p class="footer__copyright">{{ data?.footerCopyright }}</p>
+    </div>
+  </footer>
+</template>
